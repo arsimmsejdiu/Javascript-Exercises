50da1b302fb4d6a2ca3bcbe81d320a30ee51cd8c
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-05-11 14:50:41 +0200

    Finished all of the exercises

--- lvl3/js/exo30.js
+++ lvl3/js/exo30.js
@@ -3,3 +3,11 @@ let text = "Je suis un texte";
 let bool = false;
 
 //Code à faire ici, on attend plus que typeof !
+
+function typeOf(obj) {
+    return {}.toString.call(obj).split(' ')[1].slice(0, -1).toLowerCase();
+}
+
+console.log(typeOf(number));
+console.log(typeOf(text));
+console.log(typeOf(bool));
--- lvl3/js/exo32.js
+++ lvl3/js/exo32.js
@@ -1,1 +1,7 @@
 //Cpde à faire ici
+var person = {
+    name: "John",
+    age: 25
+}
+
+console.log("My name is " + person.name + " and i am " + person.age + " years old.")
