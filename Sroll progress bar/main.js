ceab8b72f51b2a2642e7b0259c857dc6b114ebfb
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-06-15 09:21:48 +0200

    scroll progress bar

--- Sroll progress bar/main.js
+++ Sroll progress bar/main.js
@@ -4,5 +4,8 @@ function scrollIndicator() {
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     var scrolled = (winScroll / height) * 100;
+    console.log(scrolled);
+    console.log(winScroll);
+    console.log(height);
     document.getElementById("bar").style.width = scrolled + "%";
 }
