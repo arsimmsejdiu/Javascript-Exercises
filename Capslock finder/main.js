c3b741d0de7f070e02589d6caebba20c41330220
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-06-23 09:31:58 +0200

    Added also num lock detector

--- Capslock finder/main.js
+++ Capslock finder/main.js
@@ -1,35 +1,35 @@
-const message = document.getElementById('warning');
-const input = document.getElementById('input');
-const warning = document.getElementById('message');
+const message = document.getElementById("warning");
+const input = document.getElementById("input");
+const warning = document.getElementById("message");
 
-input.addEventListener("keyup", function(event) { 
+input.addEventListener("keyup", function(event) {
     //⚠️ Caps Lock is activated...
     if (event.getModifierState('CapsLock')) {
-        if (event.getModifierState('NumLock')) {
-            warning.style.display = "block"
-        } else {
-            warning.style.display = "none"
-        }
         message.style.display = "block"
     } else {
         message.style.display = "none"
     }
 });
 
-// input.addEventListener("keyup", function(event) { 
-//     //⚠️ Caps Lock is activated...
-//     if (event.getModifierState('CapsLock')) {
-//         message.style.display = "block"
-//     } else {
-//         message.style.display = "none"
-//     }
-// });
+input.addEventListener("keydown", function(event) {
+     //⚠️ Caps Lock is activated...
+     if (event.getModifierState('NumLock')) {
+        warning.style.display = "block"
+    } else {
+        warning.style.display = "none"
+    }
+})
 
-// input.addEventListener("keydown", function(event) {
-//      //⚠️ Caps Lock is activated...
-//      if (event.getModifierState('NumLock')) {
-//         warning.style.display = "block"
+// input.addEventListener("keyup", function (event) {
+//   //⚠️ Caps Lock is activated...
+//   if (event.getModifierState("CapsLock")) {
+//     if (event.getModifierState("NumLock")) {
+//       warning.style.display = "block";
 //     } else {
-//         warning.style.display = "none"
+//       warning.style.display = "none";
 //     }
-// })
+//     message.style.display = "block";
+//   } else {
+//     message.style.display = "none";
+//   }
+// });
