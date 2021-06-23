883c6e5e9e5b60db33fa4382243f306e9dbaf8da
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-06-23 09:29:22 +0200

    Added also num lock detector

--- Capslock finder/main.js
+++ Capslock finder/main.js
@@ -2,6 +2,20 @@ const message = document.getElementById('warning');
 const input = document.getElementById('input');
 const warning = document.getElementById('message');
 
+input.addEventListener("keyup", function(event) { 
+    //⚠️ Caps Lock is activated...
+    if (event.getModifierState('CapsLock')) {
+        if (event.getModifierState('NumLock')) {
+            warning.style.display = "block"
+        } else {
+            warning.style.display = "none"
+        }
+        message.style.display = "block"
+    } else {
+        message.style.display = "none"
+    }
+});
+
 // input.addEventListener("keyup", function(event) { 
 //     //⚠️ Caps Lock is activated...
 //     if (event.getModifierState('CapsLock')) {
@@ -19,18 +33,3 @@ const warning = document.getElementById('message');
 //         warning.style.display = "none"
 //     }
 // })
-
-
-input.addEventListener("keyup", function(event) { 
-    //⚠️ Caps Lock is activated...
-    if (event.getModifierState('CapsLock')) {
-        if (event.getModifierState('NumLock')) {
-            warning.style.display = "block"
-        } else {
-            warning.style.display = "none"
-        }
-        message.style.display = "block"
-    } else {
-        message.style.display = "none"
-    }
-});
