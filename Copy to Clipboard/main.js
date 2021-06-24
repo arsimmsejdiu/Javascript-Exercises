4cca6d8000dc58cda4a3f5c668796c377491a126
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-06-24 10:30:54 +0200

    designed a little the app

--- Copy to Clipboard/main.js
+++ Copy to Clipboard/main.js
@@ -4,4 +4,7 @@ buttonCopy.addEventListener("click", function() {
     const area = document.querySelector('#copy-text');
     area.select();
     document.execCommand('copy')
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 });
